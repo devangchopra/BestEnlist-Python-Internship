da93231defbe926756cdf50a22a63415d91c6338
Devang Chopra <65708528+devangchopra@users.noreply.github.com>
2020-11-24 11:34:48 +0530

    Add files via upload

---
+++ Day 1/t1 examples.py
@@ -0,0 +1,52 @@
+#Examples
+
+#Printing A Value
+print("30 days 30 hour challenge")
+print('30 days 30 hour challenge')
+
+#String Initialization
+Hours = "thirty"
+print(Hours)
+
+#String Indexing
+Days = "Thirty days"
+print(Days[0])
+
+#String Slicing
+Challenge = "I will win"
+print(Challenge[7:10])
+
+#String Length
+Challenge = "I will win"
+print(len(Challenge))
+
+#Changing Case Of String
+Challenge = "I will win"
+print(Challenge.lower())
+
+
+#String Concatination
+#(Without and With Space)
+a = "30 Days"
+b = "30 hours"
+c = a + b
+print(c)
+
+a = "30 Days"
+b = "30 hours"
+c = a + " " + b
+print(c)
+
+
+#Case Fold
+#All Characters will be printed in lowercase
+text = "Thirty days and Thirty hours"
+x = text.casefold()
+print(x)
+
+
+
+
+
+
+
