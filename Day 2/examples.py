d215e0e3740694d0f7e70acb01ce8fd9a7fd7e85
Devang Chopra <65708528+devangchopra@users.noreply.github.com>
2020-11-24 12:17:50 +0530

    Add files via upload

---
+++ Day 2/examples.py
@@ -0,0 +1,24 @@
+#Examples
+
+#Data Types
+
+#(integer)
+a=10
+
+#(float)
+b=15.2
+
+#(complex)
+c=10+20j
+
+#(string)
+d="Hello"
+e='world'
+
+#(Multiple Assignment)
+f=g=h=1
+
+#(Heterogeneous Assignment)
+i,j,k=10,4.03,"bye"
+
+print(a,b,c,d,e,f,g,h,i,j,k)
---
+++ Day 2/t2.py
@@ -0,0 +1,23 @@
+a=b=c=100
+print(a,b,c)
+print("\n")
+
+a=a//10
+b=b*50
+c=c+60
+
+print(a,b,c)
+print("\n")
+
+
+str="Homes"
+print(str)
+print(str.replace("m","G"))
+#this replaces every m character with G in string but doesnot affects string
+
+print("\n")
+a=10
+b=20.52
+print(float(a))
+print(int(b))
+
