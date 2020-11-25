3a12a116bc563f6abb3f0241fd26c2b8ff9a0692
Devang Chopra <65708528+devangchopra@users.noreply.github.com>
2020-11-25 23:30:08 +0530

    Add files via upload

---
+++ Day 3/theory.py
@@ -0,0 +1,30 @@
+
+#Dictionaries
+
+D1={"Key":"Value"}
+print(D1("key"))
+del(D1("key"))
+D1("Key")="values"
+#for merging (**D1,**D2)
+
+#Inputting Key-Value Pairs
+
+Dict={}
+for i in range(5):
+      Key=input("enter key")
+      Value=input("enter value")
+      Dict[key]=values
+print(Dict)
+
+#----------------------
+
+#Sets
+#Cannot be accessed through indexing
+#Unorderd and Unindexed
+S1={"A",1,"B",20.50}
+print(S1)
+S1.add(20)
+print(S1)
+S1.remove(20)
+print(S1)
+
